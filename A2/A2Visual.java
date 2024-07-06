67bc409c58b45c279e7de1910bb605f82b5da7ff
Mahim <80307451+MM120-i@users.noreply.github.com>
2024-07-06 01:54:52 -0400

    Create A2Visual.java

---
+++ A2/A2Visual.java
@@ -0,0 +1,62 @@
+package practice;
+
+public class Main {
+	
+	public static int partition(int[] A, int low, int high) {
+		
+		int pivot = A[high];
+		int i = low - 1;
+		
+		System.out.println("Value of the high - 1 : "  + (high - 1));
+		
+		for(int j = low; j <= high - 1; j++) {
+			
+			if(A[j] <= pivot) {
+				
+				i++;
+				System.out.println("\nValue of i in the loop: " + i);
+				swap(A, i, j);
+				System.out.println("Value of j in the loop: " + j + "\n");
+			}
+		}
+	
+		swap(A, i + 1, high);
+		
+		return i + 1;
+	}
+
+    private static void swap(int[] A, int i, int j) {
+    	
+    	int temp = A[i];
+    	A[i] = A[j];
+    	A[j] = temp;
+	}
+    
+    private static void printArray(int[] A) {
+    	
+    	for(int i : A) {
+    		System.out.print(i + " ");
+    	}
+    	
+    	System.out.println();
+    }
+    
+
+	public static void main(String[] args) {
+		
+		int[] A = {3, 8, 6, 1, 5, 9, 4};
+		int low = 0;
+		int high = A.length - 1;
+		
+		System.out.println("Array before partitioning:");
+		printArray(A);
+		
+		int pivotIndex = partition(A, low, high);
+		
+		System.out.println("Array after partitioning:");
+		printArray(A);
+		
+		System.out.println("Pivot index: " + pivotIndex);
+		System.out.println("Pivot value: " + A[pivotIndex]);
+    }
+}
